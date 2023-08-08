d18b2b9b6b143fa825984dada69775141cacc594
Dhruv Gupta <dhruv035@gmail.com>
2023-08-08 17:11:52 +0530

    Updates

--- index.js
+++ index.js
@@ -1,18 +1,113 @@
-import { formatUserSummary } from '@aave/math-utils';
+import { ethers } from 'ethers';
 import {
-    UiPoolDataProvider,
-    UiIncentiveDataProvider,
-    ChainId,
-  } from '@aave/contract-helpers';
-import express from 'express'
+  UiPoolDataProvider,
+  UiIncentiveDataProvider,
+  ChainId,
+} from '@aave/contract-helpers';
+import * as markets from '@bgd-labs/aave-address-book';
 import Web3 from 'web3'
 import abi from './abi/Pool.json' assert { type: "json" };
+import { formatUserSummary, formatReserves } from '@aave/math-utils';
+import dayjs from 'dayjs';
+import express from 'express'
+
+// 'reserves' and 'userReserves' inputs from Setup section
+
+
+
+/*
+- @param `currentTimestamp` Current UNIX timestamp in seconds, Math.floor(Date.now() / 1000)
+- @param `marketReferencePriceInUsd` Input from [Fetching Protocol Data](#fetching-protocol-data), `reserves.baseCurrencyData.marketReferencePriceInUsd`
+- @param `marketReferenceCurrencyDecimals` Input from [Fetching Protocol Data](#fetching-protocol-data), `reserves.baseCurrencyData.marketReferenceCurrencyDecimals`
+- @param `userReserves` Input from [Fetching Protocol Data](#fetching-protocol-data), combination of `userReserves.userReserves` and `reserves.reservesArray`
+- @param `userEmodeCategoryId` Input from [Fetching Protocol Data](#fetching-protocol-data), `userReserves.userEmodeCategoryId`
+*/
+
 const app = express()
 const port = 3000
 
 
+const provider = new ethers.providers.JsonRpcProvider(
+  'https://eth-mainnet.public.blastapi.io',
+);
+
+//const currentAccount = '0x464C71f6c2F760DdA6093dCB91C24c39e5d6e18c';
+
+const poolDataProviderContract = new UiPoolDataProvider({
+  uiPoolDataProviderAddress: markets.AaveV3Ethereum.UI_POOL_DATA_PROVIDER,
+  provider,
+  chainId: ChainId.mainnet,
+});
+
+const incentiveDataProviderContract = new UiIncentiveDataProvider({
+  uiIncentiveDataProviderAddress:
+    markets.AaveV3Ethereum.UI_INCENTIVE_DATA_PROVIDER,
+  provider,
+  chainId: ChainId.mainnet,
+});
+
+async function fetchContractData(currentAccount) {
+  // Object containing array of pool reserves and market base currency data
+  // { reservesArray, baseCurrencyData }
+  const reserves = await poolDataProviderContract.getReservesHumanized({
+    lendingPoolAddressProvider: markets.AaveV3Ethereum.POOL_ADDRESSES_PROVIDER,
+  });
+
+  // Object containing array or users aave positions and active eMode category
+  // { userReserves, userEmodeCategoryId }
+  const userReserves = await poolDataProviderContract.getUserReservesHumanized({
+    lendingPoolAddressProvider: markets.AaveV3Ethereum.POOL_ADDRESSES_PROVIDER,
+    user: currentAccount,
+  });
+
+  // Array of incentive tokens with price feed and emission APR
+  const reserveIncentives =
+    await incentiveDataProviderContract.getReservesIncentivesDataHumanized({
+      lendingPoolAddressProvider:
+        markets.AaveV3Ethereum.POOL_ADDRESSES_PROVIDER,
+    });
+
+  // Dictionary of claimable user incentives
+  const userIncentives =
+    await incentiveDataProviderContract.getUserReservesIncentivesDataHumanized({
+      lendingPoolAddressProvider:
+        markets.AaveV3Ethereum.POOL_ADDRESSES_PROVIDER,
+      user: currentAccount,
+    });
+
+  const reservesArray = reserves.reservesData;
+const baseCurrencyData = reserves.baseCurrencyData;
+const userReservesArray = userReserves.userReserves;
+
+const currentTimestamp = dayjs().unix();
+
+const formattedPoolReserves = await formatReserves({
+  reserves: reservesArray,
+  currentTimestamp,
+  marketReferenceCurrencyDecimals:
+    baseCurrencyData.marketReferenceCurrencyDecimals,
+  marketReferencePriceInUsd: baseCurrencyData.marketReferenceCurrencyPriceInUsd,
+});
+const userSummary = formatUserSummary({
+  currentTimestamp,
+  marketReferencePriceInUsd: baseCurrencyData.marketReferenceCurrencyPriceInUsd,
+  marketReferenceCurrencyDecimals:
+    baseCurrencyData.marketReferenceCurrencyDecimals,
+  userReserves: userReservesArray,
+  formattedReserves: formattedPoolReserves,
+  userEmodeCategoryId: userReserves.userEmodeCategoryId,
+});
+const imp = userSummary.userReservesData.filter(data=>data.stableBorrowsUSD+data.variableBorrowsUSD>0)
+imp.forEach(im=>{
+  delete im.reserve
+})
+console.log("Summary Is:",imp)
+}
+
+
 const addresses=[];
 const data=[];
+const useful=[];
 
 const web3 = new Web3(
     "https://mainnet.infura.io/v3/b4d5f8243b484669b69913d1062982b9"
@@ -42,6 +137,15 @@ contract.methods
            ltv:web3.utils.fromWei(userData.ltv, 'ether'),
             healthFactor:web3.utils.fromWei(userData.healthFactor, 'ether')
           })
+          if(web3.utils.fromWei(userData.healthFactor, 'ether')<1.1)
+          useful.push({
+            collateral:web3.utils.fromWei(userData.totalCollateralBase, 'ether'),
+            debt:web3.utils.fromWei(userData.totalDebtBase, 'ether'),
+            borrow:web3.utils.fromWei(userData.availableBorrowsBase, 'ether'),
+            threshold:web3.utils.fromWei(userData.currentLiquidationThreshold, 'ether'),
+           ltv:web3.utils.fromWei(userData.ltv, 'ether'),
+            healthFactor:web3.utils.fromWei(userData.healthFactor, 'ether')
+          })
           console.log(data)
           setTimeout(()=>{},3000)
     }
@@ -63,6 +167,14 @@ app.get('/', async (req, res) => {
 })
 app.get('/data', (req,res)=>{
     res.json({data})
+})
+
+app.get('/useful',(req,res)=>{
+    res.json({useful})
+})
+app.get('/userSummary',async (req,res)=>{
+await  fetchContractData("0x4ebDE1e8ad8E205D4De268ba758e5725EaFBC6fD")
+  res.send("hi")
 })
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
